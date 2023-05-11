fcbdb99e78ed335022bebe29567f162afa0bc150
David <dverbenyi@gmail.com>
2023-05-11 15:24:38 +0100

    Create MVP for StackOverflow
    MVP of nested call test using JEST
    Showing that jest.spyOn does not want to work properly

---
+++ nested.js
@@ -0,0 +1,15 @@
+export function getUserData() {
+  console.log("CALLING API");
+  const user = {
+    name: "user",
+  };
+  setUserData(user);
+}
+
+function setUserData(user) {
+  console.log("Setting user data");
+}
+
+export const exportedForTesting = {
+  setUserData,
+};
---
+++ test/nested.test.js
@@ -0,0 +1,12 @@
+import { getUserData, exportedForTesting } from "../nested";
+
+// const { setUserData } = exportedForTesting;
+
+let storeUserDataSpy = jest.spyOn(exportedForTesting, "setUserData");
+
+describe("Get user data calls child method", () => {
+  it("should call the child method", async () => {
+    await getUserData();
+    expect(storeUserDataSpy).toHaveBeenCalledTimes(1);
+  });
+});
