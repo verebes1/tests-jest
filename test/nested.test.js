6d04eb5c8b079a9361ef6009c11e3df337e0c7a2
David <dverbenyi@gmail.com>
2023-05-11 16:34:45 +0100

    Add rewire to babel as a plugin

--- test/nested.test.js
+++ test/nested.test.js
@@ -1,8 +1,9 @@
-import * as nested from "../nested";
+import __RewireAPI__, * as nested from "../nested";
 let storeUserDataSpy = jest.spyOn(nested, "setUserData");
 
 describe("Get user data calls child method", () => {
   it("should call the child method", async () => {
+    __RewireAPI__.__Rewire__("setUserData", storeUserDataSpy);
     nested.getUserData();
     expect(storeUserDataSpy).toHaveBeenCalledTimes(1);
   });
